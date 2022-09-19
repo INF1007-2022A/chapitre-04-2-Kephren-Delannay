5ca086f52e6758061e9fe9e3033880e93d351854
Kephren <kephren.delannay.sampany@gmail.com>
2022-09-19 11:13:32 -0400

    First two exercises and attept at exercise 3

--- exercice.py
+++ exercice.py
@@ -4,13 +4,32 @@
 import random
 
 def get_first_part_of_name(name):
-	return ""
+	# split the name
+	first_name = name.split('-')[0]
+	# makes it lower to ensure a good format
+	first_name = str.lower(first_name)
+	# makes first letter uppercase
+	first_name = first_name.replace(first_name[0], first_name[0].upper())
+	return 'Bonjour ' + first_name
 
 def get_random_sentence(animals, adjectives, fruits):
-	return ""
+	animal = random.choice(animals)
+	adjective = random.choice(adjectives)
+	fruit = random.choice(fruits)
+	return (f"Aujourd'hui, j'ai vu un {animal} s'emparer d'un panier {adjective} plein de {fruit}")
 
 def encrypt(text, shift):
-	return ""
+	# makes the sentence lower to ease the calculations
+	lower_case_text = text.lower()
+	result = ""
+	# loop through the sentence
+	for char in lower_case_text:
+		# get the ASCII code
+		n = ord(char)
+		if 97 <= n <= 122:
+			# TODO: loop when n >= 123
+			result += chr(n + shift)
+	return result
 
 def decrypt(encrypted_text, shift):
 	return ""
